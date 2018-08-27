24fa3c9f1e80a71e6b22e20ec9e89990ce12c771
Hirukami <golubev.nk@gmail.com>
2018-08-27 21:30:39 +0300

    add check SElinux and user
    Signed-off-by: Hirukami <golubev.nk@gmail.com>

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+if [ $(getenforce) != "Disabled" ]; then
+   sed -i 's/\(^SELINUX=\).*/\SELINUX=disabled/' /etc/sysconfig/selinux
+   sed -i 's/\(^SELINUX=\).*/\SELINUX=disabled/' /etc/selinux/config
+   echo "SElinux disabled, pls reboot VM"
+   exit 1
+fi
+
+if [ $(whoami) != "root" ]; then
+   echo "Run script from user root"
+   exit 1
+fi
+
 yum install mercurial git tmux -y
 git clone https://github.com/Hirukami/freepbx-install.git
 cd freepbx-install
