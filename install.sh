a9866e69d3a96ccac1529801ed15dae6b41b5454
Hirukami <golubev.nk@gmail.com>
2018-08-27 12:39:52 +0300

    Update install.sh
    fix tmux console

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 yum install mercurial git tmux -y
 git clone https://github.com/Hirukami/freepbx-install.git
 cd freepbx-install
-tmux
-echo "Open tmux console"
-bash freepbx-install.sh
+tmux new-session -d -s freepbx -n freepbx
+tmux send-keys -t freepbx:freepbx "bash freepbx-install.sh" Enter
+tmux attach -t freepbx
